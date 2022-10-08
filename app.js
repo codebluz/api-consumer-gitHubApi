ab0ae9b231f5351395a8bbfc356a17f0782ba807
Bruno Luz <brunorodrigo19@gmail.com>
2022-10-08 09:39:03 -0300

    update app.listen command

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import router from './routes/route.js';
 const app = express();
 const port = process.env.port || 3000;
 
-app.listen(port, console.log(`Server is running on port ${port}`));
+app.listen(port, (req, res) => { console.log(`Server is running on port ${port}`) });
 app.use(express.json());
 app.use(router);
 
