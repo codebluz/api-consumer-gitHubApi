0aba2714608f7bbbfbdfcdfb1ef2a913eadf2579
Bruno Luz <brunorodrigo19@gmail.com>
2022-10-08 09:32:14 -0300

    update files

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+import express from 'express';
+import router from './routes/route.js';
 const app = express();
-const router = require('./router/router');
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
+app.listen(port, console.log(`Server is running on port ${port}`));
+app.use(express.json());
 app.use(router);
-app.listen(port, function(req, res) {
-    console.log("Servidor rodando na porta "+port);
-});
+
